21a29c5c12221c1e63560ab491ad86f722462da7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:06:59 +0400

    display it

--- src/StringDataDemo.java
+++ src/StringDataDemo.java
@@ -1,5 +1,6 @@
 public class StringDataDemo {
     public static void main(String[] args) {
         String employeeMessage ="Team meeting at 3PM";
+        System.out.println(employeeMessage);
     }
 }
