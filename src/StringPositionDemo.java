b4cf8a80ce09be383a5c9d3f6db8362e7a1d5fb2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:38:30 +0400

    Task 4: Find Positions and Check String Start or End in Java

--- src/StringPositionDemo.java
+++ src/StringPositionDemo.java
@@ -1,2 +1,18 @@
 public class StringPositionDemo {
+    public static void main(String[] args) {
+
+
+    String emailAddress = "codline@gmail.com";
+    System.out.println("Original Email :"+ emailAddress);
+
+        System.out.println("Position of @ :"+ emailAddress.indexOf("@"));
+        System.out.println("Position of gmail :"+ emailAddress.indexOf("gmail"));
+
+        System.out.println("Email starts with support"+ emailAddress.startsWith("codline"));
+        System.out.println("Email ends with .com"+ emailAddress.endsWith(".com"));
+
 }
+}
+
+
+
