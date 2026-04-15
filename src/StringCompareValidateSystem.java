2d0b80e8145cc82432fbf49a2f8d1f557a604cc1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 16:16:28 +0400

    added class StringCompareValidateSystem

--- src/StringCompareValidateSystem.java
+++ src/StringCompareValidateSystem.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 
 public class StringCompareValidateSystem {
+    public static void main(String[] args) {
 
-    Scanner sc=new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+        String text1 = sc.nextLine();
+        System.out.println("Original:" + text1);
+        System.out.println("Length:" + text1.length());
 
 
+    }
 }
