5ba2652638e1877d2972870c5f7af524086d31e5
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:03:55 +0400

    use length function to count the length

--- src/StringOperationsDemo.java
+++ src/StringOperationsDemo.java
@@ -1,6 +1,8 @@
 public class StringOperationsDemo {
     public static void main(String[] args) {
-        String CustomerName = "Ali";
+            String CustomerName = "Ali";
+
+        System.out.println("The Length of name" +CustomerName.length() );
 
 
     }
