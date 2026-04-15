858104d9ca57b8791f92e06952e9c3c8aef4283d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:24:07 +0400

    use tirm function

--- src/StringModifyDemo.java
+++ src/StringModifyDemo.java
@@ -1,5 +1,8 @@
 public class StringModifyDemo {
     public static void main(String[] args) {
+        String studentName = "Hala  ";
+        System.out.println("Student name :"+ studentName);
+        System.out.println("Name After remove Addtion space ..."+ studentName);
 
     }
 }
