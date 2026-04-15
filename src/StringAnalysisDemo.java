5e77468b7a23a651aca58b9ca1538bc3eb6b631e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:00:06 +0400

    indexof method

--- src/StringAnalysisDemo.java
+++ src/StringAnalysisDemo.java
@@ -7,6 +7,8 @@ public class StringAnalysisDemo {
         System.out.println("Length of feedback"+ customerFeedback.length());
         String customerFeedback2 ="Welcome to our store";
         System.out.println("Both feedbacks equal"+ customerFeedback.equals(customerFeedback2));
+        System.out.println("Position of store"+ customerFeedback.indexOf("store"));
+
 
 
     }
