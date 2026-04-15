76d377c54b4958e38e06c515ddba8f4ee18465b7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:39:03 +0400

    updated class StringMenuDemo

--- src/StringMenuDemo.java
+++ src/StringMenuDemo.java
@@ -9,6 +9,15 @@ public class StringMenuDemo {
         System.out.println("Enter a string:");
         String text = sc.nextLine();
 
+        System.out.println("Menu ==");
+        System.out.println("Enter choice (1-5):");
+        System.out.println("1. Length");
+        System.out.println("2. To Uppercase");
+        System.out.println("3. To Lowercase");
+        System.out.println("4. Contains 'java'");
+        System.out.println("5. Compare with another string");
+
+
     }
 }
 
