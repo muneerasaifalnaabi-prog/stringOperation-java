8d7a06ac709d35574d6ff717dab001bae6480ec4
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:25:49 +0400

    contains() function

--- src/StringModifyDemo.java
+++ src/StringModifyDemo.java
@@ -3,6 +3,7 @@ public class StringModifyDemo {
         String studentName = "Hala  ";
         System.out.println("Student name :"+ studentName);
         System.out.println("Name After remove Addtion space ..."+ studentName);
+        System.out.println("It is Contains name Hala"+ studentName.contains("Hala"));
 
     }
 }
