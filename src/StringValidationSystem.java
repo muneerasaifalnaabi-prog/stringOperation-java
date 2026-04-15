b308ec9b45fc082c9a235bbf97655177afc3844b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:44:13 +0400

    updated  class StringValidationSystem

--- src/StringValidationSystem.java
+++ src/StringValidationSystem.java
@@ -4,5 +4,9 @@ public class StringValidationSystem {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
+        System.out.println("Enter a string:");
+        String text=sc.nextLine();
+
+
     }
 }
