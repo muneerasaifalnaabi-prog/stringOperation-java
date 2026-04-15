f00b40dfff109d032e52e02d347fb9e49ceb6152
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:20:29 +0400

    Task 2: Extract and Compare Text Using Java String Methods completed

--- src/StringCompareDemo.java
+++ src/StringCompareDemo.java
@@ -2,6 +2,11 @@ public class StringCompareDemo {
     public static void main(String[] args) {
         String studentName = "Hala Ahmed ";
         System.out.println("Char in index 0 :" +studentName.charAt(0));
+        System.out.println("First Name  :" +studentName.substring(0,6));
+
+        String petName= "Cat";
+        System.out.println("Its equals ? "+ studentName.equals(petName));
+
 
 
     }
