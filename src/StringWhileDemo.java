9e0df3fd283e126733f548fb5d15d2db7147fc51
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:40:55 +0400

    added class StringWhileDemo

--- src/StringWhileDemo.java
+++ src/StringWhileDemo.java
@@ -1,6 +1,7 @@
 public class StringWhileDemo {
     public static void main(String[] args) {
         String text="Hello World Java Programming";
+        System.out.println("Original string:"+text);
 
 
     }
