54db0dbfec98858fb14d234519ac41327a9203a7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:04:40 +0400

    Task 14: Use Nested Loops with Java Strings completed

--- src/tringNestedLoopDemo.java
+++ src/tringNestedLoopDemo.java
@@ -30,7 +30,7 @@ public class tringNestedLoopDemo {
 
         for(int i=0;i<str1.length();i++){
             for(int j=0;j<str2.length();j++){
-                System.out.println(str1.charAt(i)+" vs "+str2.charAt(j));
+                System.out.println(str1.charAt(i) +str2.charAt(j));
             }
         }
 
