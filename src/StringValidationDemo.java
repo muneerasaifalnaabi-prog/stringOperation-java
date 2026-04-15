498ac2ddf2f845af59661b47ba3acbbbe8c089a8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:21:54 +0400

    startWith method

--- src/StringValidationDemo.java
+++ src/StringValidationDemo.java
@@ -1,7 +1,8 @@
 public class StringValidationDemo {
     public static void main(String[] args) {
-        String username ="muneera@gmail.com";
+        String username ="user_muneera@gmail.com";
         System.out.println(username);
+        System.out.println("Starts with user"+ username.startsWith("user"));
 
     }
 }
