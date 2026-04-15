8f0f013590da1ba48b1677bae4479c89773eec0b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:13:39 +0400

    display it

--- src/StringRecordDemo.java
+++ src/StringRecordDemo.java
@@ -1,6 +1,7 @@
 public class StringRecordDemo {
     public static void main(String[] args) {
         String customerRecord ="  Order Confirmed  ";
+        System.out.println(customerRecord);
 
     }
 
