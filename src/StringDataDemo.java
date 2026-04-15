3f2b4ed3fb8f19c73b5f04f477ba26eab0436654
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:07:29 +0400

    toUpperCase() method

--- src/StringDataDemo.java
+++ src/StringDataDemo.java
@@ -2,5 +2,7 @@ public class StringDataDemo {
     public static void main(String[] args) {
         String employeeMessage ="Team meeting at 3PM";
         System.out.println(employeeMessage);
+        System.out.println("Uppercase"+ employeeMessage.
+                );
     }
 }
