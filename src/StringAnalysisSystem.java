fbed458c58909b0b4c4310a13789c3349546b7cf
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:57:23 +0400

    updated class StringAnalysisSystem

--- src/StringAnalysisSystem.java
+++ src/StringAnalysisSystem.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class StringAnalysisSystem {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String text=sc.nextLine();
 
 
     }
