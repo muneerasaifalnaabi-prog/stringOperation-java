82309a7f45ffad625d13874f2472e2a97f2ac93d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 16:30:59 +0400

    updated class  CompleteStringProcessingSystem

--- src/CompleteStringProcessingSystem.java
+++ src/CompleteStringProcessingSystem.java
@@ -4,6 +4,7 @@ public class CompleteStringProcessingSystem {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string:");
 
     }
 }
