777c93dea682c4d5754e1aee74bccdb115396fca
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:18:11 +0400

    updated  class StringIfElseDemo

--- src/StringIfElseDemo.java
+++ src/StringIfElseDemo.java
@@ -1,6 +1,8 @@
 public class StringIfElseDemo {
     public static void main(String[] args) {
         String username="Muneera Saif ";
+
+        System.out.println("Username:"+username);
     }
 
 
