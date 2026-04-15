3e525a189532f6700fc20548f7b638ec65dcb995
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:43:55 +0400

    Task 5: Compare, Split, and Clean Text Using Java String Methodscompleted

--- src/StringProcessDemo.java
+++ src/StringProcessDemo.java
@@ -1,5 +1,24 @@
 public class StringProcessDemo {
     public static void main(String[] args) {
+        String employeeRecord = "  Ahmed Ali, HR, ahmed.ali@company.com  ";
+
+        System.out.println("Original employee record :"+ employeeRecord);
+
+        String cleanedRecord = employeeRecord.trim();
+        System.out.println("After trim :"+ cleanedRecord);
+
+        String[] recordParts = cleanedRecord.split(",");
+
+        System.out.println("Name part :"+ recordParts[0]);
+        System.out.println("Department part :"+ recordParts[1]);
+        System.out.println("Email part :"+ recordParts[2]);
+
+        String dept1 = "HR";
+        String dept2 = "hr";
+        System.out.println("HR equals hr ignore case :"+ dept1.equalsIgnoreCase(dept2));
+
+        String emptyField = "";
+        System.out.println("Empty field check :"+ emptyField.isEmpty());
 
     }
 }
