932726e483ce1453f8d5a35b1364c2dc83c589d6
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:28:21 +0400

    replace function

--- src/StringModifyDemo.java
+++ src/StringModifyDemo.java
@@ -1,9 +1,11 @@
 public class StringModifyDemo {
     public static void main(String[] args) {
-        String studentName = "Hala  ";
+        String studentName = "Hala Ahmed   ";
         System.out.println("Student name :"+ studentName);
         System.out.println("Name After remove Addtion space ..."+ studentName);
         System.out.println("It is Contains name Hala"+ studentName.contains("Hala"));
+        System.out.println("Replace hala with Hoor :"+studentName.replace("Hala","Hala"));
+
 
     }
 }
