18e5de20cf04ebefe226bfc17967f52cb86bcb75
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:08:15 +0400

    tolowerCase() method

--- src/StringDataDemo.java
+++ src/StringDataDemo.java
@@ -2,7 +2,7 @@ public class StringDataDemo {
     public static void main(String[] args) {
         String employeeMessage ="Team meeting at 3PM";
         System.out.println(employeeMessage);
-        System.out.println("Uppercase"+ employeeMessage.
-                );
+        System.out.println("Uppercase"+ employeeMessage.toUpperCase());
+        System.out.println("Lowercase"+ employeeMessage.toLowerCase());
     }
 }
