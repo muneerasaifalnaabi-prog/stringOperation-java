24a0c14b80f63bc6a74eafc5b890f2af6d7e5b82
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 16:24:26 +0400

    added class StringLoopValidationSystem

--- src/StringLoopValidationSystem.java
+++ src/StringLoopValidationSystem.java
@@ -7,10 +7,21 @@ public class StringLoopValidationSystem {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a string:");
-        String text=sc.nextLine();
+        String text = sc.nextLine();
 
-        System.out.println("Original:"+text);
-        System.out.println("Length:"+text.length());
+        System.out.println("Original:" + text);
+        System.out.println("Length:" + text.length());
 
-    }
-}
+        System.out.println("Each character:");
+        int vowels = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            System.out.println(ch);
+            ch = Character.toLowerCase(ch);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            }
+
+
+        }
+    }}
