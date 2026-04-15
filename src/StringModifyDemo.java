b5da437a4b497e47614b35d50d4ec1e09c91157a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:29:11 +0400

    Task 3: Search and Modify Text Using Java String Methods

--- src/StringModifyDemo.java
+++ src/StringModifyDemo.java
@@ -5,6 +5,7 @@ public class StringModifyDemo {
         System.out.println("Name After remove Addtion space ..."+ studentName);
         System.out.println("It is Contains name Hala"+ studentName.contains("Hala"));
         System.out.println("Replace hala with Hoor :"+studentName.replace("Hala","Hala"));
+        System.out.println(studentName);
 
 
     }
