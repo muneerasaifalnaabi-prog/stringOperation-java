1266bf0de033de49faf16ad1dd1a86e21836d0a9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:58:01 +0400

    updated  class tringNestedLoopDemo

--- src/tringNestedLoopDemo.java
+++ src/tringNestedLoopDemo.java
@@ -3,6 +3,9 @@ public class tringNestedLoopDemo {
         String text1="Hello";
         String text2="World";
 
+        System.out.println("String 1:"+text1);
+        System.out.println("String 2:"+text2);
+
     }
 
 
