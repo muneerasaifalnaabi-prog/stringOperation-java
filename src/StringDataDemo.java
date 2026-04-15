e322c78836e7f9f2cff356a96c935a7b2f0d02dd
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 14:09:57 +0400

    substring method

--- src/StringDataDemo.java
+++ src/StringDataDemo.java
@@ -5,5 +5,9 @@ public class StringDataDemo {
         System.out.println("Uppercase"+ employeeMessage.toUpperCase());
         System.out.println("Lowercase"+ employeeMessage.toLowerCase());
         System.out.println("Position of meeting"+ employeeMessage.indexOf("meeting"));
+        System.out.println("Extract PM"+ employeeMessage.substring(16,18));
+
+        //System.out.println("Contains 3PM"+ employeeMessage.contains("3PM"));
+
     }
 }
