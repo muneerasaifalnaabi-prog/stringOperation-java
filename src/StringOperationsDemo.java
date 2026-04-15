1c783443b23fe3157ebcc4ada8980dd62ca42216
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 12:05:54 +0400

    upper and lower case function

--- src/StringOperationsDemo.java
+++ src/StringOperationsDemo.java
@@ -3,6 +3,8 @@ public class StringOperationsDemo {
             String CustomerName = "Ali";
 
         System.out.println("The Length of name" +CustomerName.length() );
+        System.out.println(" To uppercase :" +CustomerName.toUpperCase() );
+        System.out.println(" To Lowercase  :" +CustomerName.toLowerCase() );
 
 
     }
