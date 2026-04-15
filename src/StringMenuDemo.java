30fa8d9ed3c5aa1af65c5a18f83ab6fc9ae5bbf1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:38:14 +0400

    updated class StringMenuDemo

--- src/StringMenuDemo.java
+++ src/StringMenuDemo.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
 public class StringMenuDemo {
+    public static void main(String[] args) {
 
-    Scanner sc=new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String text = sc.nextLine();
+
+    }
 }
+
