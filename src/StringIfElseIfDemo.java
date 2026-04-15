cda04f222b4bd97d0f12197a2906c6641a5ba911
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:28:36 +0400

    updated class StringIfElseIfDemo

--- src/StringIfElseIfDemo.java
+++ src/StringIfElseIfDemo.java
@@ -2,6 +2,23 @@ public class StringIfElseIfDemo {
     public static void main(String[] args) {
         String text="Muneera Saif ";
         System.out.println("Original string:"+text);
+        if(text.isEmpty()){
+            System.out.println("String is empty");
+        }
+        else if(text.length()<5){
+            System.out.println("String is short");
+        }
+        else if(text.startsWith("H")){
+            System.out.println("String starts with H");
+        }
+        else if(text.endsWith("d")){
+            System.out.println("String ends with d");
+        }
+        else if(text.equalsIgnoreCase("helloworld")){
+            System.out.println("String matches helloworld");
+        }
+
+
     }
 
 
