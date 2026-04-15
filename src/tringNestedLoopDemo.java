22902aeb4c8fed4fc2878f8a70c1c8e8f36e4062
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-15 15:03:14 +0400

    updated  class tringNestedLoopDemo

--- src/tringNestedLoopDemo.java
+++ src/tringNestedLoopDemo.java
@@ -26,6 +26,16 @@ public class tringNestedLoopDemo {
         System.out.println("Word1:"+str1);
         System.out.println("Word2:"+str2);
 
+        System.out.println("Character comparison:");
+
+        for(int i=0;i<str1.length();i++){
+            for(int j=0;j<str2.length();j++){
+                System.out.println(str1.charAt(i)+" vs "+str2.charAt(j));
+            }
+        }
+
+
+
     }
 
 
